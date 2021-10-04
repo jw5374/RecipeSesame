8cb5a5e52d59fb280ba6b0bef57bb3f282b07847
svasandani <vasandanishailesh@gmail.com>
2021-10-04 14:52:38 -0400

    Revert unwanted formatting

--- recipesesame/src/main/java/com/recipesesame/Main.java
+++ recipesesame/src/main/java/com/recipesesame/Main.java
@@ -6,34 +6,34 @@ import java.io.IOException;
 import com.recipesesame.functions.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		Scanner scan = new Scanner(System.in);
-		BufferedOutputStream out = new BufferedOutputStream(System.out);
-		out.write("Welcome to Recipe Sesame!\n".getBytes());
-		out.write("1. Write a recipe           2. Search for a recipe\n".getBytes());
-		out.write("3. List all current recipes\n".getBytes());
-		out.write("Please enter a number or \"exit\":".getBytes());
-		out.flush();
-		String input = scan.next();
-		while (input.equalsIgnoreCase("exit") != true) {
-			switch (input) {
-			case "1":
-				out.write("This is 1 ".getBytes());
-				break;
-			case "2":
-				out.write("This is 2 ".getBytes());
-				break;
-			case "3":
-				out.write("This is 3 ".getBytes());
-				break;
-			default:
-				out.write("Unrecognized command".getBytes());
-			}
-			out.flush();
-			input = scan.next();
+    public static void main( String[] args ) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        BufferedOutputStream out = new BufferedOutputStream(System.out);
+        out.write("Welcome to Recipe Sesame!\n".getBytes());
+        out.write("1. Write a recipe           2. Search for a recipe\n".getBytes());
+        out.write("3. List all current recipes\n".getBytes());
+        out.write("Please enter a number or \"exit\":".getBytes());
+        out.flush();
+        String input = scan.next();
+        while(input.equalsIgnoreCase("exit") != true) {
+            switch(input) {
+                case "1":
+                    out.write("This is 1 ".getBytes());
+                    break;
+                case "2":
+                    out.write("This is 2 ".getBytes());
+                    break;
+                case "3":
+                    out.write("This is 3 ".getBytes());
+                    break;
+                default:
+                    out.write("Unrecognized command".getBytes());
+            }
+            out.flush();
+            input = scan.next();
 
-		}
-		scan.close();
-		out.close();
-	}
+        }
+        scan.close();
+        out.close();
+    }
 }
