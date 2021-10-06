e6f4c176424c50b44568ee3cbd18451de6c225a8
svasandani <vasandanishailesh@gmail.com>
2021-10-06 11:43:39 -0400

    Add method to search all recipes

--- recipesesame/src/main/java/com/recipesesame/functions/Handlers.java
+++ recipesesame/src/main/java/com/recipesesame/functions/Handlers.java
@@ -12,4 +12,19 @@ public class Handlers {
 			System.out.println(recipes.get(i).getDisplayInfo());
 		}
 	}
+	
+	public static void searchAllRecipes(Database database, String searchKey) {
+		ArrayList<Recipe> recipes = database.getAllRecipes();
+		ArrayList<Recipe> foundRecipes = new ArrayList<Recipe>();
+		
+		for (int i = 0; i < recipes.size(); i++) {
+			if (recipes.get(i).getTitle().toLowerCase().contains(searchKey.toLowerCase())) {
+				foundRecipes.add(recipes.get(i));
+			}
+		}
+		
+		for (int i = 0; i < foundRecipes.size(); i++) {
+			System.out.println(foundRecipes.get(i).getDisplayInfo());
+		}
+	}
 }
